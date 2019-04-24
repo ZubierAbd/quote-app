37f4fa321208904b800663e16f645aa63fa1d2f4
ZubierAbd <34255944+ZubierAbd@users.noreply.github.com>
2019-04-24 15:26:13 -0400

    Finally got thing to work

--- src/app/quote-box/quote-box.component.ts
+++ src/app/quote-box/quote-box.component.ts
@@ -15,24 +15,6 @@ export class QuoteBoxComponent implements OnInit {
     this.display_quote = QUOTES[day];
   }
 
-  whatDayIsIt() {
-    if (this.day == 1) {
-      return console.log("The day is Monday");
-    } else if (this.day == 2) {
-      return console.log("The day is Tuesday");
-    } else if (this.day == 3) {
-      return console.log("The day is Wednesday");
-    } else if (this.day == 4) {
-      return console.log("The day is Thursday");
-    } else if (this.day == 5) {
-      return console.log("The day is Friday");
-    } else if (this.day == 6) {
-      return console.log("the day is Saturday");
-    } else {
-      return console.log("The day is Sunday.");
-    }
-  }
-
   dayIncrement(day) {
     day += 1;
 
